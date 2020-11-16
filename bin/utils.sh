2b481c976a312c59cb23f74c4a71c57669c58fdd
Sébastien Lamy <lamyseba@free.fr>
2020-11-16 11:12:37 +0100

    config de dev finale

--- bin/utils.sh
+++ bin/utils.sh
@@ -6,11 +6,11 @@ cd "$PROJECT_ROOT/content"
 # créer des liens ou copier à la racine du répertoire "static"
 find  -type f \
       -regextype posix-extended -iregex '.*\.(gif|html|gpx|mov|mp3|mp4|pdf|svg|ts|webm|xcf)$' \
-      -exec git-mv "{}" "$PROJECT_ROOT/static/" \;
+      -exec ln -f -s -r "{}" "$PROJECT_ROOT/static/" \;
+      # créer des liens
+
+#      -exec git mv "{}" "$PROJECT_ROOT/static/" \;
       # déplacer
 
 #      -exec cp "{}" "$PROJECT_ROOT/static/" \;
       # copier
-
-#      -exec ln -f -s -r "{}" "$PROJECT_ROOT/static/" \;
-      # créer des liens
